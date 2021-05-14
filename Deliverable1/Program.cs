ea407b1ab5ea6af0cb2ccae973c59a29630aa444
james <owsingames@yahoo.com>
2021-05-14 16:41:29 -0400

    Second commit, updated conversions of units

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -14,10 +14,11 @@ namespace Deliverable1
             while (true)
             {
                 //ask for unit type
-                Console.Write("Would you like to convert iches or feet: ");
+                Console.WriteLine("inches, feet, fidget spinners, or memes");
+                Console.Write("Please select a unit to convert: ");
                 string input = Console.ReadLine();
 
-                //if inches
+                //unit inches
                 if (input.ToLower() == "inches")
                 {
                     Console.Write("How many inches: ");
@@ -33,7 +34,7 @@ namespace Deliverable1
                     Console.WriteLine("Would you like to convert another messurement? Yes or No");
                     string userAnswer = Console.ReadLine();
                     //end while loop if no
-                    if (userAnswer.ToLower() == "yes")
+                    if (userAnswer.ToLower() == "yes" || userAnswer.ToLower() == "y")
                     {
                         Console.Clear();
                         continue;
@@ -45,13 +46,42 @@ namespace Deliverable1
                     }
                     
                 }
-                //if feet
+                //unit fidget spinner
+                else if(input.ToLower() == "fidget spinners" || input.ToLower() == "fidgets" || input.ToLower() == "fidget")
+                {
+                    Console.Write("How many fidget spinners: ");
+                    double fidgets = double.Parse(Console.ReadLine());
+
+
+                    //convert fidget spinners to new unit
+                    //3.5 fidgets = 1 inch
+                    double newValue = fidgets / 3.5;
+
+                    Console.WriteLine("{0} fidget spinners = {1} inches", fidgets, newValue);
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Would you like to convert another messurement? Yes or No");
+                    string userAnswer = Console.ReadLine();
+                    //end while loop if no
+                    if (userAnswer.ToLower() == "yes" || userAnswer.ToLower() == "y")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Goodbye");
+                        break;
+                    }
+
+                }
+                //unit feet
                 else if (input.ToLower() == "feet")
                 {
                     Console.Write("How many feet: ");
                     double feet = double.Parse(Console.ReadLine());
 
-                    //conver feet to new unit
+                    //conver feet to meme unit
                     //1 foot = 5 memes
                     double newValue = feet * 5;
                     Console.WriteLine("{0} feet = {1} memes", feet, newValue);
@@ -59,7 +89,32 @@ namespace Deliverable1
                     Console.WriteLine("Would you like to convert another messurement? Yes or No");
                     string userAnswer = Console.ReadLine();
                     //end while loop if no
-                    if (userAnswer.ToLower() == "yes")
+                    if (userAnswer.ToLower() == "yes" || userAnswer.ToLower() == "y")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Goodbye");
+                        break;
+                    }
+                }
+                else if(input.ToLower() == "meme" || input.ToLower() == "memes")
+                {
+                    Console.Write("How many memes: ");
+                    double memes = double.Parse(Console.ReadLine());
+
+                    //convert memes unit to feet
+                    // 5 meme = 1 foot
+                    double newValue = memes / 5;
+
+                    Console.WriteLine("{0} memes = {1} feet", memes, newValue);
+
+                    Console.WriteLine("Would you like to convert another messurement? Yes or No");
+                    string userAnswer = Console.ReadLine();
+                    //end while loop if no
+                    if (userAnswer.ToLower() == "yes" || userAnswer.ToLower() == "y")
                     {
                         Console.Clear();
                         continue;
@@ -72,7 +127,7 @@ namespace Deliverable1
                 }
                 else
                 {
-                    Console.WriteLine("Please enter a inches or feet");
+                    Console.WriteLine("Please enter a valid unti to convert: inches, fidget spinners, feet, or memes");
                     Console.Clear();
                 }
 
